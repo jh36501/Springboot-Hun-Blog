f140f48ac2d688252c879f010ab2a4822cd0b79b
unknown <jh36501@naver.com>
2022-06-22 18:31:54 +0900

    환경 세팅 완료

---
+++ src/main/java/com/hun/blog/HunBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hun.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HunBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hun/blog/HunBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hun.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
